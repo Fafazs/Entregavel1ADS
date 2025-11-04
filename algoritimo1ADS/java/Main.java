06fffa90efbfc095dbfce680ae055bc52e3dbdef
Fabricio Nascimento <153478556+Fafazs@users.noreply.github.com>
2025-11-04 18:48:14 -0300

    Refactor Main class for improved readability

--- algoritimo1ADS/java/Main.java
+++ algoritimo1ADS/java/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
@@ -42,4 +43,5 @@ public class Main {
         System.out.println("Array ordenado: " + Arrays.toString(sortedArr));
     }
 }
+
 

f74cdbc53c46a3c4d21afdfa84629825e9cdb573
Fernando carvalho <folicos1fawkes@gmail.com>
2025-11-04 18:57:50 -0300

    Alterando minha java class main e testando codigos javascript e java

---
+++ algoritimo1ADS/javascript/fibonacci.js
@@ -0,0 +1,12 @@
+function fibonacci(n) {
+    let sequencia = [0, 1];
+    
+    while (sequencia.length < n) {
+      let proximo = sequencia[sequencia.length - 1] + sequencia[sequencia.length - 2];
+      sequencia.push(proximo);
+    }
+    
+    return sequencia.slice(0, n);
+  }
+
+  console.log(fibonacci(10));
